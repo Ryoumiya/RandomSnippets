970be880916a47690fcad6098e22d586b9e5c274
Ryoumiya <33937105+Ryoumiya@users.noreply.github.com>
2020-04-01 11:03:27 +0700

    Create DiscordbotTestHaruBot.py
    Added DiscordBot Test As An Example

---
+++ DiscordbotTestHaruBot.py
@@ -0,0 +1,41 @@
+import discord
+
+TOKEN = ""
+GUILD = ""
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    for guild in client.guilds:
+        if guild.name == GUILD:
+            break
+
+    print(
+        f'{client.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})'
+    )
+
+    members = '\n - '.join([member.name for member in guild.members])
+    print(f'Guild Members:\n - {members}')
+
+@client.event
+async def on_message(message):
+    # we do not want the bot to reply to itself
+    if message.author == client.user:
+        return
+    
+    if message.content.startswith('!hello'):
+        response = "hello world"
+        await message.channel.send(response)
+
+    if message.content.startswith('!hey'):
+        msg = 'Hello {0.author.mention}'.format(message)
+        await message.channel.send(msg)
+
+    if message.content.startswith('!mg'):
+        #msg = 'Hello {0.author.mention}'.format(message)
+        await message.channel.send('Hello', file=discord.File('tophat.png'))
+
+
+client.run(TOKEN)
